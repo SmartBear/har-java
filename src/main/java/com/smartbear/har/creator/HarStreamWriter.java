aa7f398ef9e75a8f538ab58b91c6f4e3fee3df12
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-04-22 11:34:19 +0200

    Adds java doc

--- src/main/java/com/smartbear/har/creator/HarStreamWriter.java
+++ src/main/java/com/smartbear/har/creator/HarStreamWriter.java
@@ -4,9 +4,24 @@ import com.smartbear.har.model.HarEntry;
 
 import java.io.IOException;
 
+/**
+ * Writes HAR log to an output file in a streaming way. It should be used when entries need to be appended over time.
+ *
+ */
 public interface HarStreamWriter {
 
+    /**
+     * Adds an entry to an already created HarLog.
+     *
+     * @param      harEntry    the harEntry you want to append.
+     * @exception  IOException  if it fails to write the entry
+     */
     void addEntry(HarEntry harEntry) throws IOException;
 
+    /**
+     * Closes the entries array and the root HarLog object.
+     *
+     * @exception  IOException  if it fails to close the HarLog
+     */
     void closeHar() throws IOException;
 }
--- src/main/java/com/smartbear/har/creator/HarWriter.java
+++ src/main/java/com/smartbear/har/creator/HarWriter.java
@@ -5,6 +5,17 @@ import com.smartbear.har.model.HarLog;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Writes HAR log to an to the given file.
+ *
+ */
 public interface HarWriter {
+    /**
+     * Writes the HarLog to the provided file.
+     *
+     * @param      harLog    the harLog needs to be written.
+     * @param      logFile   the file where harlLog needs to be written.
+     * @exception  IOException  if the harLog cannot be written
+     */
     void writeHarLogToFile(HarLog harLog, File logFile) throws IOException;
 }
